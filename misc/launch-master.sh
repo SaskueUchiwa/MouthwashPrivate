363521abe2eef79ad38725f14ba527bf660c5c86
Edward Smale <essmale2005@gmail.com>
2023-08-13 14:25:19 +0100

    Fix redis config for master launch script

--- misc/launch-master.sh
+++ misc/launch-master.sh
@@ -1,9 +1,9 @@
-mkdir -p redis_conf
+mkdir -p redis-conf
+export $(cat .env | xargs)
+echo "requirepass $REDIS_PASSWORD" > redis-conf/redis.conf
+
 docker compose -p mwgg up -d
 
-echo "require_pass $REDIS_PASSWORD" > redis_conf/redis.conf
-
 cp misc/db.sql data/pg
-export $(cat .env | xargs)
 docker exec -e PGPASSWORD=$POSTGRES_PASSWORD -i mwgg-mwgg-postgres-1 psql --username=postgres -d Mouthwash -f /var/lib/postgresql/data/db.sql
 rm data/pg/db.sql
