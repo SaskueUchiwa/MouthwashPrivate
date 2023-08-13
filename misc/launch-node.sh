46026dad22e5d55ce693af070c8e22a89d09a9bc
Edward Smale <essmale2005@gmail.com>
2023-08-13 14:32:20 +0100

    Add auto socket ip discovery for master and node in Docker deployment

--- misc/launch-node.sh
+++ misc/launch-node.sh
@@ -1,2 +1,2 @@
 docker build -f Dockerfile.Node . --tag mouthwashgg-node
-docker run -d -e HINDENBURG_CLI_ARGS="--socket.port $2 --nodeId $1" --env-file .env --network mwgg_app mouthwashgg-node
+docker run -d -e HINDENBURG_CLI_ARGS="--socket.port $2 --socket.ip 'auto' --nodeId $1" --env-file .env --network mwgg_app mouthwashgg-node
