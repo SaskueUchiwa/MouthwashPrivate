3340ba88ff978fc64612f1849263df6b8a27181c
Edward Smale <essmale2005@gmail.com>
2023-09-06 15:35:39 +0100

    Add SkeldJS/data

---
+++ SkeldJS/packages/data/lib/KillDistances.ts
@@ -0,0 +1,7 @@
+import { KillDistance } from "@skeldjs/constant";
+
+export const KillDistances = {
+    [KillDistance.Short]: 1.0,
+    [KillDistance.Medium]: 1.8,
+    [KillDistance.Long]: 2.5,
+};
---
+++ SkeldJS/packages/data/lib/index.ts
@@ -0,0 +1,6 @@
+export * from "./maps";
+
+export * from "./ColorCodes";
+export * from "./DisconnectMessages";
+export * from "./KillDistances";
+export * from "./MatchmakingServers";
