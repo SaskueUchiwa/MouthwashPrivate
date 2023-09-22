8a1955eecf063ecd0d25d59c4b547f26b4dcc263
Edward Smale <essmale2005@gmail.com>
2023-09-22 14:50:08 +0100

    Remove default kill button for Impostors

---
+++ mod-bepinex-client/MouthwashClient/Patches/Game/RemoveDefaultKillButtonPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace MouthwashClient.Patches.Game
+{
+    [HarmonyPatch(typeof(ActionButton), nameof(ActionButton.ToggleVisible))]
+    public static class RemoveDefaultKillButtonPatch
+    {
+        public static bool Prefix(ActionButton __instance)
+        {
+            __instance.Hide();
+            return false;
+        }
+    }
+}
