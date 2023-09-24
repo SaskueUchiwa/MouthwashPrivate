4b7bf2d45f6380ed7ed5ec56d452b4716e2b3198
Edward Smale <essmale2005@gmail.com>
2023-09-24 20:06:58 +0100

    Add hud strings modification on client

---
+++ mod-bepinex-client/MouthwashClient/Enums/HudLocation.cs
@@ -0,0 +1,13 @@
+﻿namespace MouthwashClient.Enums
+{
+    public enum HudLocation
+    {
+        GameCode,
+        GamePlayerCount,
+        TaskText,
+        RoomTracker,
+        PingTracker,
+        TaskCompletion,
+        MeetingButtonHudText
+    }
+}
