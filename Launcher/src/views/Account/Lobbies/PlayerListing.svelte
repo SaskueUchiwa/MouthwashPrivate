6dffc3e2e2b62b94f4d906cd85b8381682aecb85
Edward Smale <essmale2005@gmail.com>
2023-10-02 02:18:04 +0100

    Use coloured heads for the lobby listings

--- Launcher/src/views/Account/Lobbies/PlayerListing.svelte
+++ Launcher/src/views/Account/Lobbies/PlayerListing.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+    import * as amongus from "@skeldjs/constant";
     import type { Player, UserLogin } from "../../../stores/accounts";
+    import ItemCanvas from "../../Preview/ItemCanvas.svelte";
 
     export let user: UserLogin;
     export let player: Player;
+
+    const playerHeadImg = new Image;
+    playerHeadImg.src = "/no-color-au-head.webp";
 </script>
 
 <div class="flex items-center gap-2 max-w-35">
-    <img alt="character head" src="/no-color-au-head.webp" width={16}>
+    <div class="w-4 h-4">
+        <ItemCanvas
+            layers={[ { img: playerHeadImg, material: "player", pivot: { x: 0, y: 0 }, scale: 1 } ]}
+            playerColor={player.cosmetic_color || amongus.Color.Red}
+            offset={ { x: 0, y: 0 }}/>
+    </div>
     <span
         class="text-xs flex flex-wrap justify-center"
         class:text-red-400={player.role_alignment === "Impostor"}
