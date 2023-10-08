b29647bdd3e3396be354f80481c7eb811a76e444
Edward Smale <essmale2005@gmail.com>
2023-10-09 00:23:55 +0100

    Fix ghost behaviour on player after is dead in gamedata being set to false

---
+++ mod-bepinex-client/MouthwashClient/Patches/Game/GameDataDeadGhostLayerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MouthwashClient.Patches.Game
+{
+    [HarmonyPatch(typeof(GameData.PlayerInfo), nameof(GameData.PlayerInfo.Deserialize))]
+    public static class GameDataDeadGhostLayerPatch
+    {
+        public static void Postfix(GameData.PlayerInfo __instance)
+        {
+            // Adapted from PlayerControl.cs
+            __instance.Object.gameObject.layer =
+                __instance.IsDead ? LayerMask.NameToLayer("Ghost") : LayerMask.NameToLayer("Players");
+
+            if (__instance.Object.AmOwner)
+            {
+                DestroyableSingleton<HudManager>.Instance.Chat.SetVisible(__instance.IsDead);
+                DestroyableSingleton<HudManager>.Instance.ShadowQuad.gameObject.SetActive(!__instance.IsDead);
+                __instance.Object.AdjustLighting();
+            }
+        }
+    }
+}
