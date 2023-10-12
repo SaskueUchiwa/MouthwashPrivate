133d42455c4f0b83c99669d1ce0159ebd2e88e49
Edward Smale <essmale2005@gmail.com>
2023-10-12 15:20:16 +0100

    Great progress on fixing chat crash bugs

---
+++ mod-bepinex-client/MouthwashClient/Patches/Game/ChatCrashDumbFixPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace MouthwashClient.Patches.Game
+{
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.OnEnable))]
+    public static class ChatCrashDumbFixPatch
+    {
+        public static void Postfix(PlayerControl __instance)
+        {
+            if (DestroyableSingleton<HudManager>.Instance != null && DestroyableSingleton<HudManager>.Instance.Chat != null)
+            {
+                DestroyableSingleton<HudManager>.Instance.Chat.chatScreen.SetActive(true);
+                DestroyableSingleton<HudManager>.Instance.Chat.chatScreen.SetActive(false);
+            }
+        }
+    }
+}
